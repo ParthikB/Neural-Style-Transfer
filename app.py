e1c5827c06c50a4ee5e88d3954e115b2f4345248
Parthik Bhandari <connect2parthik@gmail.com>
2020-09-13 14:16:10 +0530

    Caching issue resolved

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ import numpy as np
 
 from __helpers__ import *
 
+
 app = Flask(__name__)
 
 # Creating a user_file folder if not present in the working dir
@@ -14,18 +15,45 @@ if 'temp_user_files' not in os.listdir():
 	os.mkdir('temp_user_files')
 # temp_user_file : It'll contain the images uploaded by the user temporarily
 
+# app.config["CACHE_TYPE"] = "null"
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config["IMAGE_UPLOADS"] = os.path.join(os.getcwd(), 'temp_user_files')
 home_dir = os.getcwd()
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 # Defining the Home page
 @app.route('/')
 def home():
+	print('home')
+	print(os.getcwd())
+	print(os.listdir('static'))
+	if 'output.jpg' in os.listdir('static'):
+	# 	os.remove("static/output.jpg")	
+		print('Older Output found. Deleted.')
 	return render_template('index.html')
 
 
 # Defining the Prediction page
 @app.route('/', methods=['GET', 'POST'])
 def predict():
+	print('predict')
+	print(os.getcwd())
+	print(os.listdir('static'))
+	if 'output.jpg' in os.listdir('static'):
+	# 	print('Older Output found. Deleted.')
+		os.remove("static/output.jpg")
+
 	if request.method == 'POST' and request.form:
 		# Reading the POST request
 		content_img = request.files['img1name']
@@ -59,4 +87,6 @@ def predict():
 
 
 if __name__ == '__main__':
-	app.run(port=int(random.random()*10000), debug=True)
+	app.run(port=3333, debug=True)
+
+# int(random.random()*10000)
