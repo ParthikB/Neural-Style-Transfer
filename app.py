c0f1e00b170f864479c9e0fa4834370a8d246d6d
Parthik Bhandari <connect2parthik@gmail.com>
2020-09-15 03:28:45 +0530

    Added second page

--- app.py
+++ app.py
@@ -16,10 +16,11 @@ if 'temp_user_files' not in os.listdir():
 # temp_user_file : It'll contain the images uploaded by the user temporarily
 
 # app.config["CACHE_TYPE"] = "null"
-app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+# app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 app.config["IMAGE_UPLOADS"] = os.path.join(os.getcwd(), 'temp_user_files')
 home_dir = os.getcwd()
 
+# To DISABLE Caching
 @app.after_request
 def add_header(r):
     """
@@ -33,60 +34,55 @@ def add_header(r):
     return r
 
 # Defining the Home page
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-	print('home')
-	print(os.getcwd())
-	print(os.listdir('static'))
-	if 'output.jpg' in os.listdir('static'):
-	# 	os.remove("static/output.jpg")	
-		print('Older Output found. Deleted.')
 	return render_template('index.html')
 
 
 # Defining the Prediction page
-@app.route('/', methods=['GET', 'POST'])
-def predict():
-	print('predict')
-	print(os.getcwd())
-	print(os.listdir('static'))
-	if 'output.jpg' in os.listdir('static'):
-	# 	print('Older Output found. Deleted.')
-		os.remove("static/output.jpg")
+@app.route('/output', methods=['GET', 'POST'])
+def output():
 
-	if request.method == 'POST' and request.form:
-		# Reading the POST request
-		content_img = request.files['img1name']
-		style_img   = request.files['img2name']
+	if request.method == 'POST':
+		if request.form['submit'] == 'Do the magic!':
+			print(1111111111111, 'button pressed!', request.files, request.form)
+	# 		# Reading the POST request
+			content_img = request.files['img1name']
+			style_img   = request.files['img2name']
 
-		# Saving the Image file in local env
-		content_path = os.path.join(app.config["IMAGE_UPLOADS"], 'content_img.jpg')
-		style_path   = os.path.join(app.config["IMAGE_UPLOADS"], 'style_img.jpg')
+	# 		# Saving the Image file in local env
+			content_path = os.path.join(app.config["IMAGE_UPLOADS"], 'content_img.jpg')
+			style_path   = os.path.join(app.config["IMAGE_UPLOADS"], 'style_img.jpg')
+			content_img.save(content_path)
+			style_img.save(style_path)
 
-		content_img.save(content_path)
-		style_img.save(style_path)
-
-		generatedImage, losses = runStyleTransfer(content_path,
-								style_path,
-								iterations     = 2,
-								SAVE_EVERY     = 0,
-								contentWeight  = 1,
-								styleWeight    = 0.8,
-								output_dirName = 'static',
-								save_stats     = False)
+	# 		# Running Style Transfer
+			print('[INFO] Style Transfer Intializing..')
+			generatedImage, losses = runStyleTransfer(content_path,
+									style_path,
+									iterations     = 2,
+									SAVE_EVERY     = 0,
+									contentWeight  = 1,
+									styleWeight    = 0.8,
+									output_dirName = 'static',
+									save_stats     = False)
 
 
-		# Deleting the file from the database
-		os.remove(content_path)
-		os.remove(style_path)
+			# Deleting the file from the database
+			os.remove(content_path)
+			os.remove(style_path)
+			
+			# Switching back to the main dir because Current dir is changed internally
+			os.chdir(home_dir)
+			print('[INFO] Output generated.')
+			return render_template('output.html')
+	
+		print('[INFO] Rendering Output')
+		return render_template('index.html')
 		
-		# Switching back to the main dir because Current dir is changed internally
-		os.chdir(home_dir)
-		
-	return render_template('index.html')
 
 
 if __name__ == '__main__':
-	app.run(port=3333, debug=True)
+	app.run(port=4444, debug=True)
 
 # int(random.random()*10000)
